f7512acfbb4e9b1dc79997f8446864d4d8669457
Mirco Poretti <mircoporetti@gmail.com>
2020-11-15 23:52:31 +0100

    Updates

---
+++ precache-manifest.6aa9c684cb5cf8cb0a99830215f255db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f37c59826ce91a221cd4515c29eee50",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "afb08fb4257e1aecb5d6",
+    "url": "/resume/static/css/main.c3998199.chunk.css"
+  },
+  {
+    "revision": "f293bf3f9aea367527c4",
+    "url": "/resume/static/js/2.297faf3d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/resume/static/js/2.297faf3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb08fb4257e1aecb5d6",
+    "url": "/resume/static/js/main.8f06ebaf.chunk.js"
+  },
+  {
+    "revision": "110beccd39da32aa8d5e",
+    "url": "/resume/static/js/runtime-main.49716f41.js"
+  },
+  {
+    "revision": "d9136eeee2ce370671f65614679bbdaa",
+    "url": "/resume/static/media/me.d9136eee.jpg"
+  },
+  {
+    "revision": "9c450794723bd4764e49c9a43c85e2f8",
+    "url": "/resume/static/media/top_background.9c450794.jpg"
+  }
+]);
