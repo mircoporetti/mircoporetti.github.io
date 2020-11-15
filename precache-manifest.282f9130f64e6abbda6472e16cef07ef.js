2c6a31f9731e0d03acfcca4dc743168beb956f50
Mirco Poretti <mircoporetti@gmail.com>
2020-11-16 00:48:01 +0100

    Updates

---
+++ precache-manifest.282f9130f64e6abbda6472e16cef07ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c65f3b7c6c49d9ce52e39733468958c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf7447208ddaf8b5e3e2",
+    "url": "/static/css/main.b5e82b75.chunk.css"
+  },
+  {
+    "revision": "f293bf3f9aea367527c4",
+    "url": "/static/js/2.297faf3d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.297faf3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7447208ddaf8b5e3e2",
+    "url": "/static/js/main.df557a03.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "d9136eeee2ce370671f65614679bbdaa",
+    "url": "/static/media/me.d9136eee.jpg"
+  },
+  {
+    "revision": "9c450794723bd4764e49c9a43c85e2f8",
+    "url": "/static/media/top_background.9c450794.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.12c1e568621f3741682920fe272b3521.js"
+  "/precache-manifest.282f9130f64e6abbda6472e16cef07ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
