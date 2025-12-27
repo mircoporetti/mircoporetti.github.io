1349d6eb8026ca70d3b676c8ed870b3ae4f3e13c
Mirco Poretti <hello@mircoporetti.me>
2025-12-27 16:39:17 +0100

    Add experience title

--- src/Home/Timeline/Timeline.tsx
+++ src/Home/Timeline/Timeline.tsx
@@ -13,6 +13,9 @@ const Timeline: FunctionComponent<Props> = () => {
         <>
             <Grid container justifyContent={"center"} className={css.timelineGrid} spacing={2}>
                     <Grid item xs={11} sm={8} md={7}>
+                        <Typography variant={"h3"} className={css.workingExperienceTitle} align={"left"}>
+                            Working Experience
+                        </Typography>
                         <Typography variant={"h6"} className={css.timelineTitle} align={"center"}>Now</Typography>
                         <div className={css.timelineContainer}>
                             <AnimatedTimeline/>
